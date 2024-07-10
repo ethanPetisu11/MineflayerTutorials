42ba3fd38363a34914a318f35fc2ff36ddc12c18
ethanPetisu11 <cb.esb@yahoo.com>
2024-07-10 14:05:05 -0400

    Update 01-simple-bot.js

--- scripts/01-simple-bot.js
+++ scripts/01-simple-bot.js
@@ -3,7 +3,7 @@ const mineflayer = require('mineflayer');
 // Setup bot connection
 const bot = mineflayer.createBot({
   host: 'localhost',
-  port: '12345',
+  port: '6398',
   username: "Hello_world",
-  version: '1.8.9'
+  version: '1.20.6'
 });
